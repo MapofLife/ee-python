434e23cb9d1a35b64173f70093551ca7560911f6
benscarlson <ben.s.carlson@gmail.com>
2014-05-12 15:04:59 -0400

    initial commit

---
+++ src/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import ee
+import ConfigParser
+
+Config = ConfigParser.ConfigParser()
+Config.read('.ee-properties')
+
+MY_SERVICE_ACCOUNT = Config.get('Authentication', 'ServiceAccount')
+MY_PRIVATE_KEY_FILE = Config.get('Authentication', 'PrivateKeyFile')
+
+print "initializing..."
+ee.Initialize(ee.ServiceAccountCredentials(MY_SERVICE_ACCOUNT, MY_PRIVATE_KEY_FILE))
+print "initializing complete"
+ 
+fc = (ee.FeatureCollection('ft:1Ec8IWsP8asxN-ywSqgXWMuBaxI6pPaeh6hC64lA')
+      .filter(ee.Filter().eq('ECO_NAME', 'Sonoran desert')))
+ 
+print fc.getInfo()
